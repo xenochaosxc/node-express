5a61d852b16d424598e8702f615a14e8e9d78d99
xenochaosxc <134462553+xenochaosxc@users.noreply.github.com>
2023-09-11 19:41:20 -0400

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let app = express();
+let phrase = "Hello World";
+console.log(phrase);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
